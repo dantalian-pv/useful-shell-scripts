b4d3c51eeeff254491c1171804a03a9aa5946400
demon <dantalian.pv@gmail.com>
2018-10-27 11:55:15 +0200

    Add GPL License
    Signed-off-by: dantalian-pv <dantalian.pv@gmail.com>

--- screen-ocr.sh
+++ screen-ocr.sh
@@ -1,4 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
+
+# License:
+# Copyright (C) 2018 Pavel Dmitriev <dantalian.pv@gmail.com>
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+
 # Dependencies: tesseract-ocr imagemagick scrot xclip
 
 # select tesseract_lang in eng rus equ ;do break;done
